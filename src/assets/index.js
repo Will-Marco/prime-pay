ffa229cb3251013ebc8b467958fd15b79a64c4cc
Will-Marco <mr.marco0101@gmail.com>
2023-10-22 22:04:03 +0500

    Prepared to coding & added responsive Navbar

---
+++ src/assets/index.js
@@ -0,0 +1,3 @@
+export { default as logo } from "./logo.png";
+export { default as bars } from './bars.svg'
+export { default as xmark } from './xmark.svg'
---
+++ src/util/constants.js
@@ -0,0 +1,18 @@
+export const navigationLink = [
+  {
+    id: "home",
+    title: "Bosh sahifa",
+  },
+  {
+    id: "features",
+    title: "Xizmatlar",
+  },
+  {
+    id: "product",
+    title: "Mahsulotlar",
+  },
+  {
+    id: "clients",
+    title: "Mijozlar",
+  },
+];
