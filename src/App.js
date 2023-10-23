f4fae5f46089a6349a42e51abae27730f00a8b1c
Will-Marco <mr.marco0101@gmail.com>
2023-10-23 22:28:39 +0500

    Add Hero section

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import { Navbar } from "./components";
+import { Home, Navbar } from "./components";
 import { styles } from "./util/style";
 
 function App() {
   return (
     <div className="bg-primary w-full overflow-hidden">
+      {/*Navbar */}
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.container}`}>
           <Navbar />
+        </div>
+      </div>
+
+      {/* Hero section */}
+      <div className={styles.flexStart}>
+        <div className={styles.container}>
+          <Home />
         </div>
       </div>
     </div>
--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,4 @@
 export { default as logo } from "./logo.png";
-export { default as bars } from './bars.svg'
-export { default as xmark } from './xmark.svg'
+export { default as bars } from "./bars.svg";
+export { default as xmark } from "./xmark.svg";
+export { default as robot } from "./robot.png";
